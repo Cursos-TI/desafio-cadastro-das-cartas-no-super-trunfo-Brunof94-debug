862b44e6b34cfd4f5e20138bdf1f6a00aab67458
Brunof94-debug <brunoprof07@gmail.com>
2025-07-29 02:22:09 +0000

    Atualiazação do código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,24 +2,24 @@
 
 int main(){
     // Variáveis da carta 1.
-    char estado1; 
-    char codigo1[5]; 
-    char cidade1[30]; 
-    int populacao1; 
+    char estado1;
+    char codigo1[5];
+    char cidade1[30];
+    int populacao1;
     float area1;
     float pib1;
     int numPontosTuristicos1;
 
-                                    // Variáveis da carta 2.
-                                        char estado2;
-                                            char codigo2[5]; 
-                                                char cidade2[30]; 
-                                                    int populacao2; 
-                                                        float area2; 
-                                                            float pib2; 
-                                                                int numPontosTuristicos2; 
+    // Variáveis da carta 2.
+    char estado2;
+    char codigo2[5];
+    char cidade2[30];
+    int populacao2;
+    float area2;
+    float pib2;
+    int numPontosTuristicos2;
 
-    //Entrada de dados da carta 1.
+    // Entrada de dados da carta 1.
     printf("Carta 1 \n");
 
     printf("Digite o Estado: \n");
@@ -43,9 +43,9 @@ int main(){
     printf("Digite o número de pontos turísticos: \n");
     scanf(" %d", &numPontosTuristicos1);
 
-    printf("\n"); //Pular linha.
+    printf("\n"); // Pular linha.
 
-    //Entrada de dados da carta 2.
+    // Entrada de dados da carta 2.
     printf("Carta 2 \n");
 
     printf("Digite o Estado: \n");
@@ -69,9 +69,9 @@ int main(){
     printf("Digite o número de pontos turísticos: \n");
     scanf(" %d", &numPontosTuristicos2);
 
-    printf("\n"); //Pular linha.
+    printf("\n"); // Pular linha.
 
-    //Saída de dados da carta 1.
+    // Saída de dados da carta 1.
     printf("Carta 1\n");
 
     printf("Estado: %c\n", estado1);
@@ -88,15 +88,15 @@ int main(){
 
     printf("Número de pontos turísticos: %d\n", numPontosTuristicos1);
 
-    float densidade1 = (float) populacao1 / area1; //Adiciona a uma variável para densidade populacional.
+    float densidade1 = (float)populacao1 / area1; // Adiciona a uma variável para densidade populacional.
 
-    printf("Densidade populacional: %2f\n", densidade1);
+    printf("Densidade populacional: %.2f\n", densidade1);
 
-    float pibPerCapita1 = (float) pib1 / populacao1; //Adiciona uma variável para PIB per capita.
+    float pibPerCapita1 = (float)pib1 / populacao1; // Adiciona uma variável para PIB per capita.
 
-    printf("PIB per Capita: %2f\n\n", pibPerCapita1);
+    printf("PIB per Capita: %.2f\n\n", pibPerCapita1);
 
-    //Saída de dados da carta 2.
+    // Saída de dados da carta 2.
     printf("Carta 2\n");
 
     printf("Estado: %c\n", estado2);
@@ -113,13 +113,13 @@ int main(){
 
     printf("Número de pontos turísticos: %d\n", numPontosTuristicos2);
 
-   float densidade2 = (float) populacao2 / area2; //Adiciona a uma variável para densidade populacional.
+    float densidade2 = (float)populacao2 / area2; // Adiciona a uma variável para densidade populacional.
 
-    printf("Densidade populacional: %2f\n", densidade2);
+    printf("Densidade populacional: %.2f\n", densidade2);
 
-    float pibPerCapita2 = (float) pib2 / populacao2; //Adiciona uma variável para PIB per capita.
+    float pibPerCapita2 = (float)pib2 / populacao2; // Adiciona uma variável para PIB per capita.
 
-    printf("PIB per Capita: %2f\n\n", pibPerCapita2);
+    printf("PIB per Capita: %.2f\n\n", pibPerCapita2);
 
     return 0;
 }
